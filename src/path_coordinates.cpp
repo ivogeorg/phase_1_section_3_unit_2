f9d58816bf6aef23f70a68519f4575976dde5726
Ivo Georgiev <ivogeorg@gmail.com>
2024-05-14 04:18:06 +0000

    set of position vertices

---
+++ src/path_coordinates.cpp
@@ -0,0 +1,38 @@
+#include "robot_commander/robot_commander.h"
+#include "ros/ros.h"
+#include <iostream>
+#include <set>
+#include <utility>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "polygon_vertices");
+
+    RobotCommander robot;
+
+    set<pair<float, float>> vertices;
+
+    vertices.insert(make_pair(robot.get_x_position(), robot.get_y_position()));
+    robot.move_forward(3);
+    vertices.insert(make_pair(robot.get_x_position(), robot.get_y_position()));
+    robot.turn(0.2, 3);
+    vertices.insert(make_pair(robot.get_x_position(), robot.get_y_position()));
+    robot.move_forward(3);
+    vertices.insert(make_pair(robot.get_x_position(), robot.get_y_position()));
+    robot.turn(0.2, 3);
+    vertices.insert(make_pair(robot.get_x_position(), robot.get_y_position()));
+    robot.move_forward(3);
+    vertices.insert(make_pair(robot.get_x_position(), robot.get_y_position()));
+    robot.turn(0.2, 3);
+    vertices.insert(make_pair(robot.get_x_position(), robot.get_y_position()));
+    robot.move_forward(3);
+    vertices.insert(make_pair(robot.get_x_position(), robot.get_y_position()));
+
+    cout << vertices.size() << " vertices recorded:\n";
+    for (auto vertex: vertices) {
+        cout << "(" << vertex.first << ", " << vertex.second << ")\n";    
+    }
+
+    return 0;
+}
